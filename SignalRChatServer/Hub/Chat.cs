c8e68ec3a8786f9e3717431e86b162f7c01070d1
Lucas Freitas <lucas@xsys.com.br>
2022-03-04 16:12:57 -0300

    Add project files.

---
+++ SignalRChatServer/Hub/Chat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatServer
+{
+    public class Chat : Hub 
+    {
+        public void NewMessage(string userName, string message)
+        {
+            Clients.All.SendAsync("newMessage", userName, message);
+        }
+    }
+}
